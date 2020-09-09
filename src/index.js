b82b3c21a01ca74df3ec7c3368e89676979223fe
kryptondv <kryptondevpl@gmail.com>
2020-09-09 12:28:56 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GlobalProvider } from './context/context';
 import App from './components/App';
 
 import './styles/index.scss';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   
@@ -20,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
